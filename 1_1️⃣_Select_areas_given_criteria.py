d053cfd08413e9db4ffc0a83012234afa7b240f2
yifanhu-2 <61471835+yifanhu-2@users.noreply.github.com>
2022-06-23 18:34:40 -0500

    update statistics to include population-level info

--- 1_1️⃣_Select_areas_given_criteria.py
+++ 1_1️⃣_Select_areas_given_criteria.py
@@ -18,7 +18,7 @@ st.set_page_config(layout="wide")
 
 @st.cache
 def load_data(path):
-    df = pd.read_csv(path)
+    df = pd.read_csv(path, index_col=0)
     return df
 
 with urlopen('https://raw.githubusercontent.com/OpenDataDE/State-zip-code-GeoJSON/master/il_illinois_zip_codes_geo.min.json') as response:
@@ -28,6 +28,8 @@ with urlopen('https://raw.githubusercontent.com/OpenDataDE/State-zip-code-GeoJSO
 df_race = load_data('processed_data/poverty_RACE AND HISPANIC OR LATINO ORIGIN.csv')
 df_race = df_race[df_race['Stats'] == 'Estimate'].loc[df_race['Zipcode'] != 'Illinois', ~df_race.columns.isin(['Below poverty level','Stats'])] 
 
+df_pop = load_data('processed_data/poverty_POPULATION.csv')
+df_pop = df_pop[df_pop['Stats'] == 'Estimate'].loc[df_pop['Zipcode'] != 'Illinois', ~df_pop.columns.isin(['Below poverty level','Stats'])]
 
 #######################
 # choropleth map
@@ -35,42 +37,54 @@ df_race = df_race[df_race['Stats'] == 'Estimate'].loc[df_race['Zipcode'] != 'Ill
 
 # add filters
 with st.sidebar.form(key = 'choropleth-params'):
-        race_filter = st.selectbox(
-            'Select a race/ethnic group of interest:',
-            (df_race['RACE AND HISPANIC OR LATINO ORIGIN'].unique())
-        )
-
+        
         pop_thresh = st.number_input(
-            "Set a population threshold in the selected group (e.g., 10000):",
+            "Set a population threshold (e.g., 10000) for the entire zipcode area:",
             value=10000
         )
 
         pct_thresh = st.number_input(
-            'Set a threshold on percent below poverty level (e.g., 11.6):',
+            'Set a threshold on percent below poverty level (e.g., 11.6) for the entire zipcode area:',
             value=11.6
         )
 
+        race_filter = st.selectbox(
+            'Select a race/ethnic group of interest:',
+            (df_race['RACE AND HISPANIC OR LATINO ORIGIN'].unique())
+        )
+       
         submit_button = st.form_submit_button('Submit')
 
 if submit_button:
     
-    st.write('Showing zipcodes where the population for *', race_filter, '* is greater than ', pop_thresh, ', while more than ', pct_thresh, '% of this population are below poverty level:' )
+    st.write('Showing zipcodes with a total population greater than ', pop_thresh, ', and a poverty level greater than ', pct_thresh, '%.' )
+    st.write('Hover over the map to view poverty statistics for the race/ethnic group of interest: ', race_filter, '.')
 
     # filter data for display
-    dff = df_race.loc[(df_race['RACE AND HISPANIC OR LATINO ORIGIN'] == race_filter) & (df_race['Total'] >= pop_thresh) & (df_race['Percent below poverty level'] >= pct_thresh), :]
-    dff = dff.rename(columns={'RACE AND HISPANIC OR LATINO ORIGIN': 'Race/ethnicity', 'Total': 'Population'})
+    dff_pop = df_pop.loc[(df_pop['Total'] >= pop_thresh) & (df_pop['Percent below poverty level'] >= pct_thresh), :]
+    dff_pop = dff_pop.rename(columns={
+        'Total': 'Total population',
+        'Percent below poverty level': 'Total % below poverty level'
+    })
+
+    dff_race = df_race.loc[(df_race['Zipcode'].isin(dff_pop['Zipcode'])) & (df_race['RACE AND HISPANIC OR LATINO ORIGIN'] == race_filter), :]
+    dff_race = dff_race.rename(columns={
+        'Total': f'Population for {race_filter} ', 
+        'Percent below poverty level': f'% Poverty for {race_filter}'}).drop('RACE AND HISPANIC OR LATINO ORIGIN', axis=1)
+
+    dff_pop = dff_pop.merge(dff_race, how='left', on='Zipcode')
 
     with st.spinner('Loading...'):
         # plot choropleth
-        fig = px.choropleth(dff, 
+        fig = px.choropleth(dff_pop, 
                             geojson=zipcodes, locations='Zipcode', 
                             featureidkey='properties.ZCTA5CE10',
-                            color='Percent below poverty level',
+                            color='Total % below poverty level',
                             color_continuous_scale="Viridis",
-                            range_color=(df_race['Percent below poverty level'].min(), df_race['Percent below poverty level'].max()),
+                            range_color=(dff_pop['Total % below poverty level'].min(), dff_pop['Total % below poverty level'].max()),
                             scope="usa",
-                            labels={'Percent below poverty level':'Percent below poverty level'},
-                            hover_data=['Population', 'Percent below poverty level'],
+                            # hover_data=dff_pop.loc[:, dff_pop.columns != 'Zipcode'].columns.values.tolist(),
+                            hover_data=dff_pop.columns.values.tolist(),
                             height=500
                             )
         fig.update_geos(fitbounds="locations")
